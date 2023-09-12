954c4a35536e0b2007a3e3cab20256c36126d2b6
Александр Ильин <sasha8@list.ru>
2023-09-12 14:39:50 +0300

    Первый успех

--- Shop.java
+++ Shop.java
@@ -4,7 +4,11 @@
 public class Shop {
 
     public static void main(String[] args) {
-        
+
+        Toy xxx = Toy.Put("1 2 Конструктор");
+        System.out.println(xxx.id);
+        System.out.println(xxx.frequency);
+        System.out.println(xxx.name);
     }
 }
 
@@ -22,5 +26,15 @@ class Toy {
         this.id = id;
         this.name = name;
         this.frequency = frequency;
+    }
+
+    public static Toy Put(String parameters){
+        String id = parameters.substring(0, parameters.indexOf(' ')); 
+        parameters =   parameters.substring(parameters.indexOf(' ') + 1);
+        int frequency = Integer.parseInt(parameters.substring(0, parameters.indexOf(' ')));
+        parameters =   parameters.substring(parameters.indexOf(' ') + 1);
+        String name = parameters;
+
+        return new Toy(id, name, frequency);
     }
 }
