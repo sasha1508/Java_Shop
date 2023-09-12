a19609fc568ae453a0502866cb55c576a8703589
Александр Ильин <sasha8@list.ru>
2023-09-12 13:55:38 +0300

    first commit

---
+++ Shop.java
@@ -0,0 +1,26 @@
+/**
+ * main
+ */
+public class Shop {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+/**
+ * InnerShop
+ */
+class Toy {
+
+    String id;
+    String name;
+    int frequency;
+
+    public Toy(String id, String name, int frequency){
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+}
